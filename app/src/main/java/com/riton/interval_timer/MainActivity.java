5bf6da0c6c379021f9acb089ad0b9f4e90a686cb
Riton <ritonelion@outlook.com>
2019-03-16 21:42:11 +0800

    为MainAcitivity界面添加了内容，添加了新的activity用来编辑task，添加了展示活动的cardview。

--- app/src/main/java/com/riton/interval_timer/MainActivity.java
+++ app/src/main/java/com/riton/interval_timer/MainActivity.java
@@ -1,13 +1,20 @@
 package com.riton.interval_timer;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton addTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addTask = (FloatingActionButton) findViewById(R.id.add_task);
+
+
     }
 }
